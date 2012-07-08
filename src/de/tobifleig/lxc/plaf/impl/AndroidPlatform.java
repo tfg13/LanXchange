34f7d49d089d3ec07859bef5ad8830617f7dab66
tulius <tobifleig@gmail.com>
2012-07-08 17:58:43 +0200

    default activity merged with AndroidPlatform

--- src/de/tobifleig/lxc/plaf/impl/AndroidPlatform.java
+++ src/de/tobifleig/lxc/plaf/impl/AndroidPlatform.java
@@ -1,70 +1,70 @@
-/*
- * Copyright 2009, 2010, 2011, 2012 Tobias Fleig (tobifleig gmail com)
- *
- * All rights reserved.
- *
- * This file is part of LanXchange.
- *
- * LanXchange is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * LanXchange is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with LanXchange. If not, see <http://www.gnu.org/licenses/>.
- */
 package de.tobifleig.lxc.plaf.impl;
 
+import de.tobifleig.lxc.R;
+import de.tobifleig.lxc.R.layout;
+import de.tobifleig.lxc.R.menu;
 import de.tobifleig.lxc.plaf.GuiInterface;
 import de.tobifleig.lxc.plaf.Platform;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.support.v4.app.NavUtils;
 
 /**
- * Platform for Android.
- * Unfinshed.
- * No automated updates (managed by Google Play/Market)
+ * Platform for Android / Default Activity
+ *
+ * no automated updates (managed by Google Play)
  *
  * @author Tobias Fleig <tobifleig googlemail com>
  */
-public class AndroidPlatform implements Platform {
+public class AndroidPlatform extends Activity implements Platform {
 
-    @Override
-    public boolean hasAutoUpdates() {
-        return false;
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.lxc_layout);
+	}
 
-    @Override
-    public void checkAndPerformUpdates(String[] args) {
-        throw new UnsupportedOperationException("this platform does not support automatic updates!");
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.lxc_layout, menu);
+		return true;
+	}
 
-    @Override
-    public GuiInterface getGui(String[] args) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+	@Override
+	public boolean hasAutoUpdates() {
+		return false;
+	}
 
-    @Override
-    public void readConfiguration(String[] args) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+	@Override
+	public void checkAndPerformUpdates(String[] args) {
+		throw new UnsupportedOperationException(
+				"this platform does not support automatic updates!");
+	}
 
-    @Override
-    public void writeConfiguration() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+	@Override
+	public GuiInterface getGui(String[] args) {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
 
-    @Override
-    public boolean askForDownloadTargetSupported() {
-	return false;
-    }
+	@Override
+	public void readConfiguration(String[] args) {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
 
-    @Override
-    public String getDefaultDownloadTarget() {
-	throw new UnsupportedOperationException("Not supported yet.");
-    }
+	@Override
+	public void writeConfiguration() {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public boolean askForDownloadTargetSupported() {
+		return false;
+	}
+
+	@Override
+	public String getDefaultDownloadTarget() {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
 
 }
