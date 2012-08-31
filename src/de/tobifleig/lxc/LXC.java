b3a6794b72f828db975903862176da16074ff858
tulius <tobifleig@gmail.com>
2012-08-31 13:09:52 +0200

    LanXchange 1.0 RC3 (v147)

--- src/de/tobifleig/lxc/LXC.java
+++ src/de/tobifleig/lxc/LXC.java
@@ -46,7 +46,7 @@ public class LXC {
      * The internal version id.
      * For automatic updates.
      */
-    public static final int versionId = 146;
+    public static final int versionId = 147;
     /**
      * The Platform we are running on.
      */
