048c518f7b7567d9b6045757c948fb3231f8c730
tulius <tobifleig@gmail.com>
2013-07-24 21:30:46 +0200

    Autoformat + Language

--- src/de/tobifleig/lxc/plaf/impl/GenericPCPlatform.java
+++ src/de/tobifleig/lxc/plaf/impl/GenericPCPlatform.java
@@ -35,209 +35,201 @@ import javax.swing.JOptionPane;
 /**
  * A generic Platform for all OS prividing Swing and write access to the
  * LXC-directory. Offers a main-method to start LanXchange.
- * 
+ *
  * @author Tobias Fleig <tobifleig googlemail com>
  */
 public class GenericPCPlatform implements Platform {
 
-	/**
-	 * path to cfg-file.
-	 */
-	private static final String CONFIG_PATH = "lxc.cfg";
-	/**
-	 * the swing-gui.
-	 */
-	private static SwingGui gui = new SwingGui();
+    /**
+     * path to cfg-file.
+     */
+    private static final String CONFIG_PATH = "lxc.cfg";
+    /**
+     * the swing-gui.
+     */
+    private static SwingGui gui = new SwingGui();
 
-	@Override
-	public boolean hasAutoUpdates() {
-		return true;
-	}
+    @Override
+    public boolean hasAutoUpdates() {
+        return true;
+    }
 
-	@Override
-	public void checkAndPerformUpdates(String[] args) {
-		// Find out if updating is allowed, requested, forced and/or if the
-		// verification should be disabled.
-		boolean checkForUpdates = false;
-		boolean forceUpdate = false;
-		boolean overrideVerification = false;
-		boolean restartable = false;
-		if (Configuration.containsKey("allowupdates")) {
-			String result = Configuration.getStringSetting("allowupdates");
-			if ("yes".equals(result.toLowerCase())
-					|| "true".equals(result.toLowerCase())) {
-				checkForUpdates = true;
-			}
-		} else {
-			// not set yet, default to true (yes)
-			checkForUpdates = true;
-			Configuration.putStringSetting("allowupdates", "true");
-		}
-		// special settings
-		for (String s : args) {
-			if (s.equals("-update")) {
-				checkForUpdates = true;
-			} else if (s.equals("-forceupdate")) {
-				forceUpdate = true;
-			} else if (s.equals("-overrideVerification")) {
-				overrideVerification = true;
-			} else if (s.equals("-managed")) {
-				// whoever launched LXC tells us he is able to restart us in
-				// case of an update
-				restartable = true;
-			}
-		}
-		if (checkForUpdates || forceUpdate) {
-			System.out.println("Checking for Updates...");
-			// Ugly workaround, anonymous inner classes require (local)
-			// variables to be final
-			final boolean force = forceUpdate;
-			final boolean noVerification = overrideVerification;
-			final boolean managed = restartable;
-			// check in seperate thread
-			Runnable r = new Runnable() {
+    @Override
+    public void checkAndPerformUpdates(String[] args) {
+        // Find out if updating is allowed, requested, forced and/or if the
+        // verification should be disabled.
+        boolean checkForUpdates = false;
+        boolean forceUpdate = false;
+        boolean overrideVerification = false;
+        boolean restartable = false;
+        if (Configuration.containsKey("allowupdates")) {
+            String result = Configuration.getStringSetting("allowupdates");
+            if ("yes".equals(result.toLowerCase())
+                    || "true".equals(result.toLowerCase())) {
+                checkForUpdates = true;
+            }
+        } else {
+            // not set yet, default to true (yes)
+            checkForUpdates = true;
+            Configuration.putStringSetting("allowupdates", "true");
+        }
+        // special settings
+        for (String s : args) {
+            if (s.equals("-update")) {
+                checkForUpdates = true;
+            } else if (s.equals("-forceupdate")) {
+                forceUpdate = true;
+            } else if (s.equals("-overrideVerification")) {
+                overrideVerification = true;
+            } else if (s.equals("-managed")) {
+                // whoever launched LXC tells us he is able to restart us in
+                // case of an update
+                restartable = true;
+            }
+        }
+        if (checkForUpdates || forceUpdate) {
+            System.out.println("Checking for Updates...");
+            // Ugly workaround, anonymous inner classes require (local)
+            // variables to be final
+            final boolean force = forceUpdate;
+            final boolean noVerification = overrideVerification;
+            final boolean managed = restartable;
+            // check in seperate thread
+            Runnable r = new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        LXCUpdater.checkAndPerformUpdate(gui, force,
+                                noVerification, managed);
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            };
 
-				@Override
-				public void run() {
-					try {
-						LXCUpdater.checkAndPerformUpdate(gui, force,
-								noVerification, managed);
-					} catch (Exception ex) {
-						ex.printStackTrace();
-					}
-				}
-			};
+            Thread t = new Thread(r);
+            t.setName("updatecheck");
+            t.setDaemon(true);
+            t.start();
+        } else {
+            System.out
+                    .println("Not checking for updates. (disabled via lxc.cfg)");
+        }
+    }
 
-			Thread t = new Thread(r);
-			t.setName("updatecheck");
-			t.setDaemon(true);
-			t.start();
-		} else {
-			System.out
-					.println("Not checking for updates. (disabled via lxc.cfg)");
-		}
-	}
+    @Override
+    public GuiInterface getGui(String[] args) {
+        return gui;
+    }
 
-	@Override
-	public GuiInterface getGui(String[] args) {
-		return gui;
-	}
+    @Override
+    public void readConfiguration(String[] args) {
+        BufferedReader reader = null;
+        try {
+            File cfgFile = new File(CONFIG_PATH);
+            // create on first start
+            if (!cfgFile.exists()) {
+                cfgFile.createNewFile();
+            }
+            reader = new BufferedReader(new FileReader(cfgFile));
+            String zeile;
+            int i = 0; // line number
+            while ((zeile = reader.readLine()) != null) {
+                // read line after line, add content to Configuration
+                int indexgleich = zeile.indexOf('='); // search for "="
+                if (indexgleich == -1) {
+                } else {
+                    String v1 = zeile.substring(0, indexgleich); // prefix (before=)
+                    String v2 = zeile.substring(indexgleich + 1); // suffix (after =)
+                    Configuration.putStringSetting(v1, v2);
+                }
+            }
+        } catch (FileNotFoundException e1) {
+            // this is serious. exit
+            System.out.println("Cannot read/write configfile (cfg.txt)");
+            // display warning (gui not available yet)
+            JOptionPane.showMessageDialog(new JFrame(), "Cannot write to own folder. Please move to your home directory or start as administrator.",
+                    "Error", JOptionPane.ERROR_MESSAGE);
+            System.exit(1);
+        } catch (IOException e2) {
+            e2.printStackTrace();
+            System.exit(1);
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException ex) {
+                // ignore
+            }
+        }
+    }
 
-	@Override
-	public void readConfiguration(String[] args) {
-		BufferedReader reader = null;
-		try {
-			File cfgFile = new File(CONFIG_PATH);
-			// create on first start
-			if (!cfgFile.exists()) {
-				cfgFile.createNewFile();
-			}
-			reader = new BufferedReader(new FileReader(cfgFile));
-			String zeile;
-			int i = 0; // line number
-			while ((zeile = reader.readLine()) != null) {
-				// read line after line, add content to Configuration
-				int indexgleich = zeile.indexOf('='); // search for "="
-				if (indexgleich == -1) {
-				} else {
-					String v1 = zeile.substring(0, indexgleich); // prefix
-																	// (before
-																	// =)
-					String v2 = zeile.substring(indexgleich + 1); // suffix
-																	// (after =)
-					Configuration.putStringSetting(v1, v2);
-				}
-			}
-		} catch (FileNotFoundException e1) {
-			// this is serious. exit
-			System.out.println("Cannot read/write configfile (cfg.txt)");
-			// display warning (gui not available yet)
-			JOptionPane
-					.showMessageDialog(
-							new JFrame(),
-							"Cannot write to own folder. Please move to your home directory or start as administrator.",
-							"Error", JOptionPane.ERROR_MESSAGE);
-			System.exit(1);
-		} catch (IOException e2) {
-			e2.printStackTrace();
-			System.exit(1);
-		} finally {
-			try {
-				if (reader != null) {
-					reader.close();
-				}
-			} catch (IOException ex) {
-				// ignore
-			}
-		}
-	}
+    @Override
+    public void writeConfiguration() {
+        FileWriter writer = null;
+        try {
+            writer = new FileWriter(CONFIG_PATH);
+            Iterator<String> iter = Configuration.getKeyIterator();
+            while (iter.hasNext()) {
+                String s = iter.next();
+                writer.append(s.toString() + "="
+                        + Configuration.getStringSetting(s) + '\n');
+            }
+        } catch (IOException ex) {
+            System.out.println("CRITICAL: FEHLER BEIM SCHREIBEN DES LOGFILES!");
+        } finally {
+            try {
+                if (writer != null) {
+                    writer.close();
+                }
+            } catch (IOException ex) {
+            }
+        }
+    }
 
-	@Override
-	public void writeConfiguration() {
-		FileWriter writer = null;
-		try {
-			writer = new FileWriter(CONFIG_PATH);
-			Iterator<String> iter = Configuration.getKeyIterator();
-			while (iter.hasNext()) {
-				String s = iter.next();
-				writer.append(s.toString() + "="
-						+ Configuration.getStringSetting(s) + '\n');
-			}
-		} catch (IOException ex) {
-			System.out.println("CRITICAL: FEHLER BEIM SCHREIBEN DES LOGFILES!");
-		} finally {
-			try {
-				if (writer != null) {
-					writer.close();
-				}
-			} catch (IOException ex) {
-			}
-		}
-	}
+    @Override
+    public boolean askForDownloadTargetSupported() {
+        return true;
+    }
 
-	@Override
-	public boolean askForDownloadTargetSupported() {
-		return true;
-	}
+    @Override
+    public String getDefaultDownloadTarget() {
+        // depends on confituration
+        if (!Configuration.containsKey("defaulttarget")) {
+            return null; // default = always ask
+        } else {
+            // check if valid
+            File file = new File(
+                    Configuration.getStringSetting("defaulttarget"));
+            if (file.isDirectory() && file.canWrite()) {
+                return file.getAbsolutePath();
+            }
+        }
+        return null; // invalid path = ask
+    }
 
-	@Override
-	public String getDefaultDownloadTarget() {
-		// depends on confituration
-		if (!Configuration.containsKey("defaulttarget")) {
-			return null; // default = always ask
-		} else {
-			// check if valid
-			File file = new File(
-					Configuration.getStringSetting("defaulttarget"));
-			if (file.isDirectory() && file.canWrite()) {
-				return file.getAbsolutePath();
-			}
-		}
-		return null; // invalid path = ask
-	}
-
-	/**
-	 * Starts LanXchange on PC plaforms.
-	 * 
-	 * @param args
-	 *            any arguments you want to pass to LanXchange
-	 */
-	public static void main(String[] args) {
-		// check permissions for own folder. will be removed soon
-
-		File home = new File(".");
-		if (!home.canWrite()) {
-			System.out.println("ERROR: Cannot write to own directory ("
-					+ home.getAbsolutePath()
-					+ "). Try running LXC in your home directory.");
-			gui.showError("Cannot write to own folder. Please move to your home directory or start as administrator.");
-			System.exit(1);
-		}
-		LXC lxc = new LXC(new GenericPCPlatform(), args);
-	}
+    /**
+     * Starts LanXchange on PC plaforms.
+     *
+     * @param args
+     * any arguments you want to pass to LanXchange
+     */
+    public static void main(String[] args) {
+        // check permissions for own folder. will be removed soon
+        File home = new File(".");
+        if (!home.canWrite()) {
+            System.out.println("ERROR: Cannot write to my directory ("
+                    + home.getAbsolutePath()
+                    + "). Try running LXC in your home directory.");
+            gui.showError("Cannot write to my folder. Please move to your home directory or start as administrator.");
+            System.exit(1);
+        }
+        LXC lxc = new LXC(new GenericPCPlatform(), args);
+    }
 
     @Override
     public void downloadComplete(LXCFile file) {
-	// not required for generic pcs
+        // not required for generic pcs
     }
 }
