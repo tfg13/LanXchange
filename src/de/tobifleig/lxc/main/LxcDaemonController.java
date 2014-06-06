ac79f9d231f0695c369c33807a88ea9315ee81c2
nikithase <mekhar@gmx.de>
2014-06-06 15:07:42 +0200

    Implemented basic communication betweeen daemon and daemoncontroller.

--- src/de/tobifleig/lxc/main/LxcDaemonController.java
+++ src/de/tobifleig/lxc/main/LxcDaemonController.java
@@ -2,6 +2,11 @@ package de.tobifleig.lxc.main;
 
 import de.tobifleig.lxc.plaf.impl.GenericPCPlatform;
 import de.tobifleig.lxc.plaf.impl.textbased.LXCDaemonUserInterface;
+import de.tobifleig.lxc.plaf.impl.textbased.LxcDaemon;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 
 /**
@@ -17,15 +22,54 @@ public class LxcDaemonController {
     private Socket socket;
 
     public LxcDaemonController(String args[]) {
+        try {
+            socket = new Socket();
+            socket.connect(new InetSocketAddress("localhost", LxcDaemon.LXC_SERVER_PORT));
+        } catch (IOException ex) {
+            // cant connect, lxcd propably isnt running
+        }
         switch (args[1]) {
             case "status":
-                System.out.println("not yet implemented");
+                if (socket.isConnected()) {
+
+                    System.out.println("LxcDaemon is running.");
+
+                } else {
+                    System.out.println("LxcDaemon is NOT running.");
+                }
                 break;
             case "start":
-                System.out.println("not yet implemented");
+                try {
+                    ProcessBuilder builder = new ProcessBuilder();
+                    builder.environment().putAll(System.getenv());
+                    builder.command("java", "-jar", "LXC.jar", "-nogui", "daemonize");
+                    builder.start();
+                } catch (IOException ex) {
+                    System.out.println("Error starting lxc daemon");
+                    ex.printStackTrace();
+                }
+                break;
+            case "daemonize":
+                LxcDaemon daemon = new LxcDaemon();
                 break;
             case "stop":
-                System.out.println("not yet implemented");
+                if (socket.isConnected()) {
+                    try {
+                        socket.getOutputStream().write("stop\n".getBytes());
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        String answer = reader.readLine();
+                        if (answer.equals("stopping...")) {
+                            System.out.println("LXC Daemon shutting down.");
+                        } else {
+                            System.out.println("Error stopping daemon.");
+                        }
+                        socket.close();
+                    } catch (IOException ex) {
+                        System.out.println("Error stopping daemon.");
+                    }
+                } else {
+                    System.out.println("LXC Daemon is not running.");
+                }
                 break;
             case "upload-file":
                 System.out.println("not yet implemented");
---
+++ src/de/tobifleig/lxc/plaf/impl/textbased/LxcDaemon.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2009, 2010, 2011, 2012, 2013, 2014 Tobias Fleig (tobifleig gmail com)
+ *
+ * All rights reserved.
+ *
+ * This file is part of LanXchange.
+ *
+ * LanXchange is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LanXchange is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LanXchange. If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.tobifleig.lxc.plaf.impl.textbased;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * The LXC Daemon runs LXC in the backgroud and talks to the LxcDaemonController over a socket.
+ *
+ * @author Michael
+ */
+public class LxcDaemon implements Runnable {
+
+    public static int LXC_SERVER_PORT = 12345;
+
+    private ServerSocket serverSocket;
+    private Thread serverThread;
+
+    public LxcDaemon() {
+        try {
+            serverSocket = new ServerSocket(LXC_SERVER_PORT);
+            serverThread = new Thread(this);
+            serverThread.setName("LxcDaemonThread");
+            serverThread.start();
+            System.out.println("LxcDaemon started successfully.");
+        } catch (IOException ex) {
+            System.out.println("Error starting socket on port " + LXC_SERVER_PORT);
+            System.exit(1);
+        }
+    }
+
+    @Override
+    public void run() {
+        boolean run = true;
+        while (run) {
+            try {
+                Socket socket = serverSocket.accept();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                String[] commands = reader.readLine().split(" ");
+
+                switch (commands[0]) {
+                    case "status":
+                        socket.getOutputStream().write("lxcd running".getBytes());
+                        socket.close();
+                        break;
+                    case "stop":
+                        socket.getOutputStream().write("stopping...".getBytes());
+                        socket.close();
+                        System.exit(0);
+                        break;
+                    default:
+                        socket.getOutputStream().write(("unknown command: " + commands[0]).getBytes());
+                        socket.close();
+                }
+
+            } catch (IOException ex) {
+                // Connection closed before a command was sent
+                // lets just ignore this and go on with the next request
+            }
+        }
+    }
+
+}
