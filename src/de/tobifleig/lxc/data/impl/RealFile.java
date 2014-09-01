54aaa0a96d8072f191edb4022c4fd8acf38da336
tulius <tobifleig@gmail.com>
2014-09-01 15:09:25 +0200

    better handling for unreadable files during size calculation

--- src/de/tobifleig/lxc/data/impl/RealFile.java
+++ src/de/tobifleig/lxc/data/impl/RealFile.java
@@ -77,6 +77,7 @@ public class RealFile extends VirtualFile {
 
     /**
      * Returns the real file backing this virtual file.
+     *
      * @return the real file
      */
     public File getBackingFile() {
@@ -111,8 +112,12 @@ public class RealFile extends VirtualFile {
         }
         File[] content = file.listFiles();
         ArrayList<VirtualFile> result = new ArrayList<VirtualFile>();
-        for (File contentFile : content) {
-            result.add(new RealFile(contentFile, getTransferPath()));
+        if (content != null) {
+            for (File contentFile : content) {
+                result.add(new RealFile(contentFile, getTransferPath()));
+            }
+        } else {
+            System.out.println("Cannot read " + file.getAbsolutePath());
         }
         return result;
     }
