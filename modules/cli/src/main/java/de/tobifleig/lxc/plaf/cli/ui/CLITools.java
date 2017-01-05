4fcb6013ad553a077e28bb446facb46c5cb3f5a1
tfg13 <tobifleig@gmail.com>
2017-01-05 23:09:14 +0100

    cli: missing license headers

--- modules/cli/src/main/java/de/tobifleig/lxc/plaf/cli/ui/CLITools.java
+++ modules/cli/src/main/java/de/tobifleig/lxc/plaf/cli/ui/CLITools.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2017 Tobias Fleig (tobifleig gmail com)
+ * All rights reserved.
+ *
+ * Usage of this source code is governed by the GNU GENERAL PUBLIC LICENSE version 3 or (at your option) any later version.
+ * For the full license text see the file COPYING or https://github.com/tfg13/LanXchange/blob/master/COPYING
+ */
 package de.tobifleig.lxc.plaf.cli.ui;
 
 import java.io.FileDescriptor;
